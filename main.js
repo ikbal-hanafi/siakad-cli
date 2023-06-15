06d41ed0674a3f139f5bfef88b13a45f5701f4aa
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-06-15 10:19:37 +0800

    y

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const {default: login} = require('./absen.js')
+const {default: wa} = require('./wa.js')
+
+app.get('/cekabsen', async (req, res) => {
+  const r = await login('D0121505', 'D0121505')
+  if(r)
+      wa(`hallo semuanya silahkan absen\n*${r.mk}*: ${msg}`, "6285341748143-1633925671@g.us")
+  res.send(`${r}`)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening`)
+})
