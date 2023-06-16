fa16c306acac1cb38be36f7b7ec7201de49db2ce
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-06-16 11:00:44 +0800

    up

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const app = express()
 const {default: login} = require('./absen.js')
 const {default: wa} = require('./wa.js')
 
-app.get('/cekabsen/:nim/:pw/:gcid', async (req, res) => {
+app.get('/cekabsen/:nim/:pw', async (req, res) => {
   var nim = req.params.nim
   var pw = req.params.pw
-  var gcid = req.params.gcid
+  var gcid = '6285341748143-1633925671@g.us' // custom
   const msg = await login(nim, pw)
   if(msg !== '')
       wa(`hallo semuanya silahkan absen\n*${msg}`, gcid)
