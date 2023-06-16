8e74d924c52ddbc4c54547f95cfcc8b23191e90d
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-06-16 19:48:47 +0800

    up

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.get('/cekabsen/:nim/:pw', async (req, res) => {
   var pw = req.params.pw
   var gcid = process.env.gcid
   const msg = await login(nim, pw)
-  if(msg !== '')
+  if(msg !== ''){
     msg = `hallo semuanya silahkan absen\n*${msg}*`
     await wa(msg, gcid)
   }
